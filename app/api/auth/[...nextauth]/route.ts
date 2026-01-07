0db49397533f08ba0f803e06bc836591d0dfa0c0
Maor Argaman <weccelerate@gmail.com>
2026-01-07 19:07:52 +0200

    Fix build script and auth handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,4 @@
-// שים לב לתוספת של /next כאן - זה קריטי עבור ה-Handler ב-App Router
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
